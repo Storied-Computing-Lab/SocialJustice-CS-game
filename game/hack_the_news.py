46f67c85e8af2972b46a0de8e0e404decbcbf3f2
develop4learners <cqr2@cornell.edu>
2019-07-21 17:13:40 -0700

    added a hack_the_news challenge, just some pseudo-code ideas

---
+++ game/hack_the_news.py
@@ -0,0 +1,17 @@
+label hack_the_news:
+    python:
+        """
+        You have 3 hours to get the word out.
+        The Navy has internal connections at these companies who have all agreed to mark news about the accident as "censored" or "fake news"
+        We have to disable these labels from our content
+
+        Choose which social media do you want to prioritize:
+            Twidda
+            Instashare
+            Tumbly
+            WeTube
+            FaceBud
+
+        Trophies: Descriptive Statistics for people being hit with the news, people's reactions (like, angry, heart, sad), qualitative comments rolling in
+
+        """
