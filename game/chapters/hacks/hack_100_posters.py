414d822aa31e0999b50e6339f7a86b07a9fccca6
develop4learners <cqr2@cornell.edu>
2020-05-18 15:18:29 -0700

    fixed the instructions in hack_100_posters to have a few reminders and back to the default setting

--- game/chapters/hacks/hack_100_posters.py
+++ game/chapters/hacks/hack_100_posters.py
@@ -5,11 +5,20 @@ def main():
     print_poster_once()
     print_poster_once()
 
-    #This code will do something 5 times
-    for i in range(100):
-        print_poster_once()
+    #The following code will do something 5 times:
+    """
+    for i in range(5):
+        print("hey")
         print(i)
+    """
     #print("Hello inside hack_100_posters main function")
     #What is changing each time in the loop? What's staying the same?
     #Remember to use shift + o to see what prints out to the console!
     return
+
+    #Remember, the computer doesn't read comments.
+    #this is a 1-line comment
+    """
+    This is a multi-line
+    comment
+    """
