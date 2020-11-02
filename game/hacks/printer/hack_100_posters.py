9cdefdee8f26ca4baad3ae0f9d7d5cf244b20683
develop4learners <cqr2@cornell.edu>
2020-11-01 23:38:12 -0800

    added instructions to hack2 added dialogue to prompt users to look at the notebook after they earned pages

--- game/hacks/printer/hack_100_posters.py
+++ game/hacks/printer/hack_100_posters.py
@@ -1,24 +1,25 @@
 from __future__ import unicode_literals
 
+#Remember, the computer doesn't read comments.
+#this is a 1-line comment
+"""
+This is a multi-line
+comment
+"""
+
 def main():
-
+    #What do these two lines of code do?
     print_poster_once()
     print_poster_once()
 
-    #The following code will do something 5 times:
+    #Delete the """ to uncomment the for loop code.
+    #This for loop code will do something 5 times:
     """
     for i in range(5):
         print("hey")
         print(i)
     """
-    #print("Hello inside hack_100_posters main function")
-    #What is changing each time in the loop? What's staying the same?
+    #What is changing each time in this loop? What's staying the same?
+    #Remember CLICK or Spacebar to run this code!
     #Remember to use shift + o to see what prints out to the console!
     return
-
-    #Remember, the computer doesn't read comments.
-    #this is a 1-line comment
-    """
-    This is a multi-line
-    comment
-    """
