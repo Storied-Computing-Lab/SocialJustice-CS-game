5866e3b22b720114e413a245bab005ab600675dc
Aidan Tan <aidan.tan8@gmail.com>
2020-12-01 14:08:57 -0800

    adding images for hack 1

---
+++ game/hacks/ClarasComputer/virus.py
@@ -0,0 +1,11 @@
+class Virus:
+    def __init__(self):
+        self.message = "Niko wuz here"
+
+    def change_message(self, message):
+        self.message = message
+
+    def invade(self):
+        print(self.message)
+
+    
