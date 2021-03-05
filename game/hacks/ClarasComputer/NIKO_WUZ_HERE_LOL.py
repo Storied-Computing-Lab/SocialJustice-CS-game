9b93be9f9900cb766612933fc278aeac94bdad89
develop4learners <cqr2@cornell.edu>
2021-03-05 13:58:46 -0800

    Revises comments within NIKO_WUZ_HERE_LOL.py and Virus.py so they are a consistent style and less wordy.
    Hopefully this new format will be friendly to beginners! I standardized the docstrings that describe each method. I also included a 'How to use:' in each docstring so beginners do not have to guess how these methods are used. I also just made a few tweaks to Zaynab's language here and there so it would be extremely beginner-friendly, basically giving them instructions on how to succeed, but also noting for students who are interested 'There are many ways to solve this puzzle!' in Virus.py.

--- game/hacks/ClarasComputer/NIKO_WUZ_HERE_LOL.py
+++ game/hacks/ClarasComputer/NIKO_WUZ_HERE_LOL.py
@@ -9,7 +9,7 @@ def main():
     Looks like this command, niko_virus.invade(), is outputting an ANNOYING message in the 'console'!
     Press shift + o inside the game to view this annoying mesage in the console!
         What does this virus currently print out?
-        One way to unlock your computer is by changing this virus message to a less annoying one.
+        One way to unlock Clara's computer is by changing this virus message to a less annoying one.
 
     All this text is a comment. Computers do not read between the """ """
     Press command + s to save changes here.
@@ -18,14 +18,13 @@ def main():
     """
 
     niko_virus.invade() #What does this command do?
-
-    #This is also a comment
-    #BONUS: Can you make the virus say nothing at all?
+    #Hint: try putting eliminate_virus() right after this line. What happens?
     return
 
-#Tip: Other commands like eliminate_virus() might be helpful.
-#Tip: The in-game notebook has more examples of how to write a code command.
-#eliminate_virus(): changes the message of  nico_virus to "Virus eliminated"
 def eliminate_virus():
+    """
+    Changes the message of  nico_virus to "Virus eliminated".
+    How to use: write eliminate_virus() immediately after niko_virus.invade()
+    """
     global niko_virus
     niko_virus.change_message("Virus eliminated")
